cba6707153c42a15895a2266d566065d0baa4c09
Gavin <bangong_mail@163.com>
2026-05-23 20:19:48 +0800

    Delete dangong directory

--- dangong/start-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo 正在启动烦恼弹弓账户服务...
-echo.
-node server.js
-echo.
-echo 服务已停止。按任意键关闭窗口。
-pause >nul
--- dangong/start-server.ps1
+++
@@ -1,2 +0,0 @@
-Set-Location -LiteralPath $PSScriptRoot
-node server.js
